7044674e3bb9231b4e470211116d5682f4fd4459
Oclemy <oclemmi@gmail.com>
2016-11-20 05:12:05 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simpel ListView Filter
+This is a simple tutorial on how to filter a ListView.
+The ListView is populated via an array.We use a searchview as our search component.
+
+## Overview
+- Fill ListView from an array.
+- Handle searchview's onQeryTextChangeListener.
+- Our ArrayAdapter instance gives us a Filter object through the getFilter() method,we can easily use this to Filter our ListView.
+- If you want to check more advanced versions of Filtering like Custom ListView or filtering databases like MySQL,visit our website http://camposha.info
+
+![]()
+
+## Author
+Oclemy,cheers.
+http://camposha.info
---
+++ src/com/tutorials/simplelistviewfilter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.tutorials.simplelistviewfilter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+
+
+public class MainActivity extends Activity {
+
+	ListView lv;
+	SearchView sv;
+	
+    String[] teams={"Man Utd","Man City","Chelsea","Arsenal","Liverpool","Totenham"};
+	ArrayAdapter<String> adapter;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        lv=(ListView) findViewById(R.id.listView1);
+        sv=(SearchView) findViewById(R.id.searchView1);
+        
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,teams);
+        lv.setAdapter(adapter);
+        
+        sv.setOnQueryTextListener(new OnQueryTextListener() {
+			
+			@Override
+			public boolean onQueryTextSubmit(String text) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String text) {
+				
+               adapter.getFilter().filter(text);
+				
+				return false;
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
